e705a7c707b8b90a5909a2df7822ce3ec224442e
Robert Niimi <Robert Niimi>
2015-01-04 21:12:06 -0800

    made only selected marker visible

--- public/js/controllers/searchCtrl.js
+++ public/js/controllers/searchCtrl.js
@@ -1,9 +1,13 @@
 angular.module('hungry.search', [])
 
 .controller('SearchController', function($scope, $state, $http, $stateParams) {
-  // $scope.markerEvents = {};
-
   var init = function() {
+    $scope.map = {
+      center: { latitude: 37.783748, longitude: -122.409046 },
+      zoom: 14
+    };
+
+    $scope.markerControl = {};
     $scope.markers = getMarkers($scope.data);
   };
 
@@ -12,31 +16,67 @@ angular.module('hungry.search', [])
     var markersArray = [];
     for (var i = 0; i < data.length; i++) {
       markersArray.push({
-      id: i,
-      latitude: data[i].coordinates.latitude,
-      longitude: data[i].coordinates.longitude,
-      google_id: data[i].google_id
+        id: i,
+        latitude: data[i].coordinates.latitude,
+        longitude: data[i].coordinates.longitude,
+        google_id: data[i].google_id,
+        title: data[i].business_name
       });
     }
     return markersArray;
   };
 
+  // REFERENCE
+  // $scope.map.markers.push({
+  //     id: value[3],
+  //     latitude: angular.fromJson(value[1]),
+  //     longitude: angular.fromJson(value[2]),
+  //     icon: iconimg,
+  //     title : value[0],
+  //     options: {
+  //         labelContent : dist + '<br />'+$filter('date')(date,'d-M'),
+  //         labelAnchor: "36 61",
+  //         labelClass: 'labelClass',
+  //         labelStyle: newstyle,
+  //         labelInBackground: false
+  //     }
+  //  });
+
   init();
 
   $scope.markerEvents = {
     mouseover: function(marker) {
-      console.log('mouseenter');
-      console.log('marker: ', marker);
       angular.element(document.querySelector('#list_' + marker.model.google_id)).addClass('highlighted');
     },
     mouseout: function(marker) {
       angular.element(document.querySelector('#list_' + marker.model.google_id)).removeClass('highlighted');
-      console.log('mouseleave');
     },
     click: function(marker) {
-      console.log('stateparams filternum: ', $stateParams.filterNum);
       $state.go('home.business', { google_id: marker.model.google_id, filterNum: $stateParams.filterNum });
     }
+  };
+
+  $scope.businessListEventHandler = function(listEvent) {
+    var id = listEvent.target.id.slice(5); // removes 'list_' from id to get the google_id
+    var markers = $scope.markerControl.getGMarkers();
+
+    var allButSelectedIsVisible = function(isVisible) {
+      for (var i = 0; i < markers.length; i++) {
+        if (markers[i].model.google_id !== id) {
+          markers[i].setVisible(isVisible);
+        }
+      }
+    };
+
+    switch (listEvent.type) {
+      case "mouseover":
+        allButSelectedIsVisible(false);
+        break;
+      case "mouseleave":
+        allButSelectedIsVisible(true);
+        break;
+    }
+    var map = angular.element(document.querySelector('.angular-google-map'));
   };
 
   // $scope.markerOptions = {
@@ -44,8 +84,4 @@ angular.module('hungry.search', [])
   // }
 
 
-  $scope.map = {
-    center: { latitude: 37.783748, longitude: -122.409046 },
-    zoom: 14
-  };
 });
