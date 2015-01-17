c630651329ff989dd49db60e0e81f3c67fd72212
Robert Niimi <Robert Niimi>
2015-01-16 16:46:43 -0800

    fixed business_list error

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -2,108 +2,22 @@ angular.module('hungry.home', [])
 
 .controller('HomeController', function($scope, $state, $http, $stateParams) {
   $scope.searchItems = ['hungry', 'thirsty', 'studious'];
-  $scope.showHeaderIcons = true;
-  $scope.business_list;
 
   var init = function($scope) {
     // sets up css for home screen
     if ($state.is('home')) {
       angular.element(document.querySelector('header.home')).addClass('home-header');
       angular.element(document.querySelector('.background-gradient')).addClass('background-gradient-home').removeClass('background-gradient');
-      $scope.showHeaderIcons = false;
     };
-
-    //checks if user is logged in
-    loggedIn();
 
     // persists the filter value
     $scope.searchItem = setFilterValue($scope.searchItems, $stateParams);
 
-    // gets business data
-    getData();
+    //checks if user is logged in
+    loggedIn();
 
     // initally don't show modal
     $scope.show_modal = false;
-  };
-
-
-  var filterBusinesses = function(businesses, filterNum) {
-    var businessArray = [];
-    // console.log('businesses: ', businesses);
-    for (var i = 0; i < businesses.length; i++) {
-      if (businesses[i].filter.indexOf(filterNum) !== -1) {
-        businessArray.push(businesses[i]);
-      }
-    }
-    return businessArray;
-  };
-
-  var setBusinessList = function(filterNum) {
-    switch (filterNum) {
-      case '0':
-        console.log('case 0');
-        $scope.business_list = $scope.placesToEat;
-        break;
-      case '1':
-        console.log('case 1');
-        $scope.business_list = $scope.placesToDrink;
-        break;
-      case '2':
-        console.log('case 2');
-        $scope.business_list = $scope.placesToStudy;
-        break;
-    }
-  };
-
-  // makes http request to get array of businesses
-  var getData = function() {
-    $http.get('/business').
-      success(function(data, status, headers, config) {
-        $scope.placesToEat = filterBusinesses(data, '0');
-        $scope.placesToDrink = filterBusinesses(data, '1');
-        $scope.placesToStudy = filterBusinesses(data, '2');
-        $scope.business_list = setBusinessList($stateParams.filterNum);
-        $scope.markers = getMarkers($scope.business_list);
-      }).
-      error(function(data, status, headers, config) {
-        console.error('error getting business data');
-      });
-  };
-
-  // creates and returns array of business coordinates
-  var getMarkers = function(data) {
-    var markersArray = [];
-
-    var hackReactor = {
-      id: 0,
-      latitude: 37.783748,
-      longitude: -122.409046,
-      google_id: 'mothership',
-      title: 'Hack Reactor'
-    };
-
-    markersArray.push(hackReactor);
-
-    for (var i = 0; i < data.length; i++) {
-      markersArray.push({
-        id: i + 1,
-        latitude: data[i].coordinates.latitude,
-        longitude: data[i].coordinates.longitude,
-        google_id: data[i].google_id,
-        title: data[i].business_name
-      });
-    }
-    return markersArray;
-  };
-
-  var loggedIn = function() {
-    $http.get('/logged-in').
-      success(function(data, status, headers, config) {
-        $scope.isLoggedIn = data.result;
-      }).
-      error(function(data, status, headers, config) {
-        console.error('error getting authentication data');
-      });
   };
 
   // persits the filter value
@@ -114,20 +28,29 @@ angular.module('hungry.home', [])
       return filtersArray[0];
   };
 
-  init($scope);
+  // checks if user is logged in
+  var loggedIn = function() {
+    $http.get('/logged-in').
+      success(function(data, status, headers, config) {
+        $scope.isLoggedIn = data.result;
+      }).
+      error(function(data, status, headers, config) {
+        console.error('error getting authentication data');
+      });
+  };
 
-////// MODAL
+  // shows the modal
   $scope.showModal = function() {
     $scope.show_modal = true;
   };
 
+  // hides the modal
   $scope.closeModal = function(){
     $scope.show_modal = false;
   };
-////// END MODAL
 
+  // starts search on enter
   $scope.startSearch = function(keyEvent, filterName) {
-    $scope.showHeaderIcons = true;
     if (keyEvent.which === 13) {
       $state.go('home.search', { filterNum: $scope.searchItems.indexOf(filterName) });
       angular.element(document.querySelector('header.home')).removeClass('home-header');
@@ -140,4 +63,5 @@ angular.module('hungry.home', [])
     return new Array(x);
   };
 
+  init($scope);
 });
--- public/js/controllers/searchCtrl.js
+++ public/js/controllers/searchCtrl.js
@@ -2,35 +2,92 @@ angular.module('hungry.search', [])
 
 .controller('SearchController', function($scope, $state, $http, $stateParams) {
   $scope.markerControl = {};
+  $scope.business_list;
 
   var init = function() {
+    // default map setting (Hack Reactor in center)
     $scope.map = {
       center: { latitude: 37.783748, longitude: -122.409046 },
       zoom: 14
     };
 
+    // gets business data
+    getData();
   };
 
+  // filters businesses based off of their filter value
+  var filterBusinesses = function(businesses, filterNum) {
+    var businessArray = [];
+    // console.log('businesses: ', businesses);
+    for (var i = 0; i < businesses.length; i++) {
+      if (businesses[i].filter.indexOf(filterNum) !== -1) {
+        businessArray.push(businesses[i]);
+      }
+    }
+    return businessArray;
+  };
 
+  // changes business list depending on the filter
+  var setBusinessList = function(filterNum) {
+    switch (filterNum) {
+      case '0':
+        // console.log('case 0');
+        return $scope.placesToEat;
+        break;
+      case '1':
+        // console.log('case 1');
+        return $scope.placesToDrink;
+        break;
+      case '2':
+        // console.log('case 2');
+        return $scope.placesToStudy;
+        break;
+    }
+  };
 
-  // REFERENCE
-  // $scope.map.markers.push({
-  //     id: value[3],
-  //     latitude: angular.fromJson(value[1]),
-  //     longitude: angular.fromJson(value[2]),
-  //     icon: iconimg,
-  //     title : value[0],
-  //     options: {
-  //         labelContent : dist + '<br />'+$filter('date')(date,'d-M'),
-  //         labelAnchor: "36 61",
-  //         labelClass: 'labelClass',
-  //         labelStyle: newstyle,
-  //         labelInBackground: false
-  //     }
-  //  });
+  // makes http request to get array of businesses
+  var getData = function() {
+    $http.get('/business').
+      success(function(data, status, headers, config) {
+        $scope.placesToEat = filterBusinesses(data, '0');
+        $scope.placesToDrink = filterBusinesses(data, '1');
+        $scope.placesToStudy = filterBusinesses(data, '2');
+        $scope.business_list = setBusinessList($stateParams.filterNum);
+        $scope.markers = getMarkers($scope.business_list);
+      }).
+      error(function(data, status, headers, config) {
+        console.error('error getting business data');
+      });
+  };
+
+  // creates and returns array of business coordinates
+  var getMarkers = function(data) {
+    var markersArray = [];
+    var hackReactor = {
+      id: 0,
+      latitude: 37.783748,
+      longitude: -122.409046,
+      google_id: 'mothership',
+      title: 'Hack Reactor'
+    };
+
+    markersArray.push(hackReactor);
+
+    for (var i = 0; i < data.length; i++) {
+      markersArray.push({
+        id: i + 1,
+        latitude: data[i].coordinates.latitude,
+        longitude: data[i].coordinates.longitude,
+        google_id: data[i].google_id,
+        title: data[i].business_name
+      });
+    }
+    return markersArray;
+  };
 
   init();
 
+  // list of marker events
   $scope.markerEvents = {
     mouseover: function(marker) {
       angular.element(document.querySelector('#list_' + marker.model.google_id)).addClass('highlighted');
@@ -43,6 +100,7 @@ angular.module('hungry.search', [])
     }
   };
 
+  //
   $scope.businessListEventHandler = function(listEvent) {
     var id = listEvent.target.id.slice(5); // removes 'list_' from id to get the google_id
     var markers = $scope.markerControl.getGMarkers();
@@ -62,14 +120,9 @@ angular.module('hungry.search', [])
       case "mouseleave":
         allButSelectedIsNotHidden(true);
         break;
-    }
+    };
 
     var map = angular.element(document.querySelector('.angular-google-map'));
   };
-
-  // $scope.markerOptions = {
-  //   place
-  // }
-
 
 });
